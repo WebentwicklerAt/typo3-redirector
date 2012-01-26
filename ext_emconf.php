7f15aa1281490467d2d4d46da40bcb0f8a50c31f
Gernot Leitgab <leitgab@gmail.com>
2012-01-26 15:40:23 +0100

    first commit

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "redirector".
+#
+# Auto generated 07-05-2011 12:44
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Redirector',
+	'description' => 'Redirects the user to a given page (e.g. after login/logout) or to a login page if a restricted page is accessed.',
+	'category' => 'misc',
+	'author' => 'Gernot Leitgab',
+	'author_email' => 'leitgab@gmail.com',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"8b1b";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"1c6e";s:14:"ext_tables.php";s:4:"3569";s:16:"locallang_db.xml";s:4:"ff96";s:19:"doc/wizard_form.dat";s:4:"6744";s:20:"doc/wizard_form.html";s:4:"1cf9";s:31:"pi1/class.tx_redirector_pi1.php";s:4:"1dfb";s:17:"pi1/locallang.xml";s:4:"0a25";}',
+);
+
+?>
---
+++ ext_tables.php
@@ -0,0 +1,16 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key,pages';
+
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:' . $_EXTKEY . '/flexform_ds.xml');
+
+t3lib_extMgm::addPlugin(array(
+	'LLL:EXT:redirector/locallang_db.xml:tt_content.list_type_pi1',
+	$_EXTKEY . '_pi1',
+	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif',
+), 'list_type');
